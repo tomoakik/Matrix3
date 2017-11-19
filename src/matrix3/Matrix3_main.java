1302cb620908eba15ebdaba3655d1c0d53d63d15
50616013 <50616013@50616013-pc>
2017-11-19 22:27:04 +0900

    Matrix3

---
+++ src/matrix3/Matrix3_main.java
@@ -0,0 +1,22 @@
+package matrix3;
+
+import java.util.Arrays;
+
+public class Matrix3_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a[][] = {{1,2},{-1,1},};
+		double b[][] = {{3,-1},{2,-1}};
+		double c[][] = {{3,1,1,2},{5,1,3,4},{2,0,1,0},{1,3,2,1}};
+		double d[][] = { { 1, 1, 1, 1 }, { 1, 1, 1, -1 }, { 1, 1, -1, 1 }, { 1, -1, 1, 1 } };
+		double e[] = { 0, 4, -4, 2 };
+		Matrix3_lib mlib = new Matrix3_lib();
+		System.out.println("Product = "+Arrays.deepToString(mlib.getProduct(a,b)));
+		System.out.println("Cofactor = "+(mlib.getCofactor(c)));
+		System.out.println("Inverse ="+Arrays.deepToString(mlib.getInverse(c)));
+		System.out.println("Solution = "+ Arrays.toString(mlib.getSolution(d, e)));
+
+	}
+
+}
